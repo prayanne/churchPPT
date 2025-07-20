3dcf4cc5861a633f1c91b25b27bed7182be3da49
prayanne <kyhy0321@gmail.com>
2025-07-20 14:25:43 +0900

    0720

--- parse_hwp.py
+++ parse_hwp.py
@@ -1,5 +1,5 @@
 import win32com.client as win32
 
 hwp=win32.gencache.EnsureDispatch("HWPFrame.HwpObject")
-hwp.Open("/" + "2025.7.6","HWP","forceopen:true")
+hwp.Open("/src/hwp/" + "2025.7.6","HWP","forceopen:true")
 
